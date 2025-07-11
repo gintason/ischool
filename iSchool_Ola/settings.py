dd1c1a14ef9679782b78e850d52976fc11642a77
gintason <gintason@gmail.com>
2025-07-11 02:03:30 +0100

    CORS Allowed Origins updated now

--- iSchool_Ola/settings.py
+++ iSchool_Ola/settings.py
@@ -146,11 +146,9 @@ DEFAULT_FROM_EMAIL = 'noreply@ischool.ng'
 CONTACT_EMAIL = "admin@ischool.ng"
 
 # CORS settings
-CORS_ALLOWED_ORIGINS = [
-    "https://www.ischool.ng",
-   
-  
-]
+CORS_ALLOWED_ORIGINS = ["https://www.ischool.ng"]
+CORS_ORIGIN_WHITELIST = ["https://www.ischool.ng"]
+
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
